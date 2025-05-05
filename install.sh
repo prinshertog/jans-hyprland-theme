3aaa37e460c8425c1f229caa66911a18d6410619
Jan Deploige <jan@freedombox.be>
2025-05-05 17:57:51 +0200

    Update to script

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ sudo pacman -S hyprland pipewire wireplumber grim slurp hyprpaper kitty wl-clipb
 yay -S xdg-desktop-portal-hyprland-git rofi
 
 # Copy hyprland and hyprpaper configs
-cp -R hypr ∼/.config/
+cp -R hypr ~/.config/
 
 # Copy kitty config
-cp -R kitty ∼/.config/
+cp -R kitty ~/.config/
 
 # Copy fastfetch config
-cp -R fastfetch ∼/.config/
+cp -R fastfetch ~/.config/
 
 # Copy rofi config
-cp -R rofi ∼/.config/
+cp -R rofi ~/.config/
